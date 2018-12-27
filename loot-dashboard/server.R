b89b3fe3f169a6f90cf103906464f4899b7e672c
RAG-home <rgilmore@umc.edu>
2018-12-27 16:10:13 -0600

    Updated working authorization code.

--- loot-dashboard/server.R
+++ loot-dashboard/server.R
@@ -2,21 +2,36 @@ library(shiny)
 library(shinydashboard)
 library(googleAuthR)
 library(googlesheets)
-# server <- function(input, output, session) {
-#   sign_ins <- shiny::callModule(googleSignIn, "login")
-#   }
+
 server <- function(input, output, session) {
   ## Make a button to link to Google auth screen
   ## If auth_code is returned then don't show login button
-  output$loginButton <- renderUI({
+  ## Get auth code from return URL
+  access_token  <- reactive({
+    ## gets all the parameters in the URL. The auth code should be one of them.
+    pars <- parseQueryString(session$clientData$url_search)
+    
+    if (length(pars$code) > 0) {
+      ## extract the authorization code
+      gs_webapp_get_token(auth_code = pars$code)
+    } else {
+      NULL
+    }
+  })
+  
+  observeEvent(input$menu,{
     if (is.null(isolate(access_token()))) {
+      showModal(modalDialog(
       tags$a("Login",
              href = gs_webapp_auth_url(),
-             class = "btn btn-default")
+             class = "btn btn-default"), 
+      title = "Google Login"
+      ))
     } else {
       return()
     }
-  })
+  },
+  ignoreNULL = TRUE)
   
   output$logoutButton <- renderUI({
     if (!is.null(access_token())) {
@@ -45,6 +60,7 @@ server <- function(input, output, session) {
   gsLs <- reactive({
     gs_ls()
   })
+  
   user_info <- reactive({
     validate(
       need(!is.null(access_token()), message = FALSE)
@@ -55,19 +71,14 @@ server <- function(input, output, session) {
   output$currentUser <- renderUI({
     validate(
       need(!is.null(access_token()),
-           message = "No user is currently authorized.")
+           message = "No user is \ncurrently \nauthorized.")
     )
     x <- user_info()
-    line1 <- paste("displayName:", x$displayName)
-    line2 <- paste("emailAddress:", x$emailAddress)
-    line3 <- paste("date:", format(x$date, usetz = TRUE))
+    line1 <- sprintf("Welcome </br><h>back </br><h>%s!", x$user$displayName)
+
     
-    HTML(paste(line1, line2, line3, sep = "</br><h>"))
+    HTML(paste(line1, sep = "</br><h>"))
   })
-  # access_token <- callModule(googleAuth, "loginButton")
-  
-  # output$g_name = renderText({ sign_ins()$name })
-  # output$g_email = renderText({ sign_ins()$email })
-  # output$g_image = renderUI({ img(src=sign_ins()$image) })
+
   
 }
--- loot-dashboard/ui.R
+++ loot-dashboard/ui.R
@@ -6,16 +6,19 @@ options(googleAuthR.webapp.client_id =  "498958135112-hd4udv449lprp85j5e5qr1ugvg
 options(googleAuthR.webapp.client_secret = "xo58T9kE442ckGCWJkmzEP-F")
 #googlesheets::gs_auth(new_user = TRUE)
 
+
 dashboardPage(
-  dashboardHeader(title = "Loot Dashboard"),
+  dashboardHeader(title = "Loot Dashboard",
+    uiOutput("logoutButton")),
   dashboardSidebar(
     sidebarMenu(
       id = "menu",
       menuItem("Loot Overview", tabName = "loot-overivew", icon = icon("table")),
       menuItem("New Encounter", tabName = "new-encounter", icon = icon("dice")),
-      menuItem("Claim Your Loot", tabName = "loot-claimer", icon = icon("hand-holding-usd"))
+      menuItem("Claim Your Loot", tabName = "loot-claimer", icon = icon("hand-holding-usd")),
+      wellPanel(span(h3(uiOutput("currentUser")), style = "color:black", align="center"))
     )
   ),
-  dashboardBody(uiOutput("loginButton"),uiOutput("logoutButton"))
+  dashboardBody()
 )
 
