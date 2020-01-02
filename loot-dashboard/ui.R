6b444e6e0ad4ac1fc07f6f46ed41630d60790ee4
Robert A. Gilmore <rgilmore@umc.edu>
2020-01-02 15:46:24 -0600

    Changed order of sidebard menus.

--- loot-dashboard/ui.R
+++ loot-dashboard/ui.R
@@ -12,9 +12,9 @@ dashboardPage(
       id = "menu",
       menuItem("Instructions", tabName = "instructions"),
       menuItem("Campaign Selection", tabName = "campaign-selection"),
-      menuItem("Loot Overview", tabName = "loot-overivew", icon = icon("table")),
       menuItem("New Encounter", tabName = "new-encounter", icon = icon("dice")),
       menuItem("Claim Your Loot", tabName = "loot-claimer", icon = icon("hand-holding-usd")),
+      menuItem("Loot Overview", tabName = "loot-overivew", icon = icon("table")),
       wellPanel(span(h5(uiOutput("currentUser")), style = "color:black", align="center"))
     )
   ),
