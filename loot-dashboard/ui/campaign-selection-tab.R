7e2992a8caa97ad3d3716ca1a941ce9238671b39
Robert A. Gilmore <rgilmore@umc.edu>
2020-01-10 14:47:01 -0600

    Added the Add Characters box and Load Campaign Box.

--- loot-dashboard/ui/campaign-selection-tab.R
+++ loot-dashboard/ui/campaign-selection-tab.R
@@ -1,13 +1,24 @@
 tabItem(tabName = "campaign-selection", 
         fluidRow(
           box(strong(h2("New Campaign")),
-              textInput(inputId = "pc-name", label = "Player Character Name"),
-              textInput(inputId = "dm-name", label = "Dungeon Master Name"),
               textInput(inputId = "campaign-name", label = "Campaign Name"),
+              textInput(inputId = "dm-name", label = "Dungeon Master Name"),
               htmlOutput("adventure-path-output"),
               htmlOutput("adventure-output"),
               actionButton(inputId = "create-campaign", "Create Campaign")
-              
+              ),
+          box(strong(h2("Add Characters")),
+              htmlOutput("campaign-output-add"),
+              textInput(inputId = "added-first-name", label = "Player's First Name"),
+              textInput(inputId = "added-last-name", label = "Player's Last Name"),
+              textInput(inputId = "added-pc", label = "Player's Character"),
+              actionButton(inputId = "add-player-button", "Add")
+              ),
+          ),
+        fluidRow(
+          box(strong(h2("Load Campaign")),
+              htmlOutput("campaign-output-load"),
+              actionButton(inputId = "load-campaign-button", "Load")
               )
-          )
+        )
         )
