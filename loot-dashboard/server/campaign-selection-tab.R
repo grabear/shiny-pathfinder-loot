78b529c0a10f157537c4bd9d881eedf40cda3b87
Robert A. Gilmore <rgilmore@umc.edu>
2020-01-17 17:06:00 -0600

    Updated campaign-selection tab with new reactive values for loot data.

--- loot-dashboard/server/campaign-selection-tab.R
+++ loot-dashboard/server/campaign-selection-tab.R
@@ -1,22 +1,22 @@
 # An input for the user to select the adventure path.
 output$`adventure-path-output` <- renderUI({
-  selectInput(inputId = "adventure-path-selection", label = "Select an Adventure Path", choices = ap_list)
+  selectInput(inputId = "adventure-path-selection", label = "Select an Adventure Path", choices = loot_data$ap_names)
 })
 
 # An input for the user to select an adventure based on the adventure path that was selected.
 output$`adventure-output` <- renderUI({
-  a_data <- dplyr::filter(ap_data, ap_data$`Adventure Path Name` == input$`adventure-path-selection`)
+  a_data <- dplyr::filter(loot_data$ap_table, loot_data$ap_table$`Adventure Path Name` == input$`adventure-path-selection`)
   selectInput(inputId = "adventure-selection", label = "Select an Adventure", choices = c(a_data$Adventure, "Unknown"))
 })
 
 output$`campaign-output-load` <- renderUI({
-  player_camps <- dplyr::filter(players, player$emailAddress %in% players$`Email Address`)
-  player_camps <- dplyr::filter(campaigns, campaigns$ID %in% player_camps$`Campaign ID`)
+  player_camps <- dplyr::filter(loot_data$players_table, loot_data$drive_user$emailAddress %in% loot_data$players_table$`Email Address`)
+  player_camps <- dplyr::filter(loot_data$campaigns_table, loot_data$campaigns_table$ID %in% player_camps$`Campaign ID`)
   selectInput(inputId = "players-campaign-load", "Select a Campaign", choices = player_camps$`Campaign Name`)
 })
 
 output$`campaign-output-add` <- renderUI({
-  player_camps <- dplyr::filter(campaigns, campaigns$`Owner Email` %in% player$emailAddress)
+  player_camps <- dplyr::filter(loot_data$campaigns_table, loot_data$campaigns_table$`Owner Email` %in% loot_data$drive_user$emailAddress)
   selectInput(inputId = "players-campaign-add", "Select a Campaign", choices = player_camps$`Campaign Name`)
 })
 
